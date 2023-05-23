fd7211ce2ee74688c265d9f470f7953aa8a366b0
AlexJWhitcomb <133329470+AlexJWhitcomb@users.noreply.github.com>
2023-05-23 11:36:05 -0700

    Add files via upload

---
+++ TODO-EX/index.js
@@ -0,0 +1,25 @@
+document.querySelector('.click').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Don't forget to add a task!!");
+    }
+
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        let current_tasks = document.querySelectorAll(".delete");
+        for(let i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+    }
+}
